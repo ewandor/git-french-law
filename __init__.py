5525cc5396da02ea9d8239cdaa96ffdf619f4c3d
Guillaume Gentile <ewandor@dorfsvald.net>
2014-03-27 19:53:27 +0100

    fetching the versions of the articles

--- __init__.py
+++ __init__.py
@@ -1,39 +1,65 @@
 from mechanize import Browser
-from pyquery import PyQuery as Q
 
+from page import ConstitutionPage, ArticlePage
 class LegifranceClient(object):
 
     host = 'http://www.legifrance.gouv.fr/'
 
     def __init__(self):
+        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)'
+        self.__init_browser()
+
+    def __init_browser(self):
         self.browser = Browser()
+        self.browser.set_handle_robots(False)
+        self.browser.addheaders = [('User-agent', self.user_agent)]
+
 
     def get_page(self, page):
-        self.browser.open(self.host + page.adress)
-        return page(self.browser.response().read())
+        self.browser.open(self.host + page.get_adress())
+        page.set_content(self.browser.response().read())
+        return page
 
     def get_constitution(self):
-        articles = {}
-        return self.get_page(ConstitutionPage)
+        constitution = []
+        page = self.get_page(ConstitutionPage())
+        article_list = page.get_article_list()
 
-class Page(object):
+        for article_id in article_list:
+            article = Article()
+            page = self.get_page(ArticlePage(ConstitutionPage, article_id))
+            article_version_list = page.get_article_version_list()
+            for version_id in article_version_list:
+                page = self.get_page(ArticlePage(ConstitutionPage, article_id, version_id))
+                version = Version()
+                page.set_article_version(version)
+                article.add_version(version)
 
-    adress = ''
+            constitution.append(article)
 
-    def __init__(self, content):
-        self.content = content
-        self.dom = Q(content)
-
-    def set_articles(self, articles):
+    def get_text(self, text_id):
         pass
 
+    def get_article_list(self, text_id):
+        pass
 
-class ConstitutionPage(Page):
+    def get_article_version_list(self, text_id, article_id):
+        pass
 
-    adress = 'affichTexte.do?cidTexte=LEGITEXT000006071194'
+    def get_article_version(self, text_id, article_id, date):
+        pass
 
 class Article(object):
+
+    def __init__(self):
+        self.versions = []
+
+    def add_version(self, version):
+        self.versions.append(version)
+
+class Version(object):
     pass
 
 cli = LegifranceClient()
-print cli.get_constitution().dom('#subcontent')
+constitution = cli.get_constitution()
+print constitution
---
+++ page.py
@@ -0,0 +1,70 @@
+from pyquery import PyQuery as Q
+from urllib import urlencode
+from urlparse import parse_qs
+
+class LegiFrancePage(object):
+    adress = ''
+
+    def set_content(self, content):
+        self.content = content
+        self.dom = Q(content)
+        self.data = Q(self.dom('.data'))
+
+    def set_articles(self, articles_data):
+        for article_html in self.data('.article'):
+            article_dom = Q(article_html)
+            articles_data.append(
+                {'title': article_dom('.titreArt'), 'body': [paragraph.text for paragraph in article_dom('p')]})
+            article_id_list.append(article_dom('.histoArt .liensArtResolu'))
+
+
+class TextPage(LegiFrancePage):
+    action = 'affichTexte.do'
+    cidText = ''
+
+    def get_adress(self):
+        return self.action + '?' + urlencode({'cidTexte': self.cidText})
+
+
+class ConstitutionPage(TextPage):
+    cidText = 'LEGITEXT000006071194'
+
+    def get_article_list(self):
+        article_list = []
+        for link in self.dom('.titreArt a'):
+            query = Q(link).attr('href').split('?')[1]
+            article_list.append(parse_qs(query)['idArticle'][0])
+        return article_list
+
+
+class ArticlePage(LegiFrancePage):
+    def __init__(self, text_page, article_id, date=None):
+        self.text_page = text_page
+        self.idArticle = article_id
+        self.dateTexte = date
+
+    def get_adress(self):
+        if self.dateTexte is None:
+            return 'affichTexteArticle.do?' + urlencode(
+                {'cidTexte': self.text_page.cidText, 'idArticle': self.idArticle}
+                )
+        else:
+            return 'affichTexteArticle.do?' + urlencode(
+                {'cidTexte': self.text_page.cidText, 'idArticle': self.idArticle, 'dateTexte': self.dateTexte}
+                )
+
+    def set_article_version(self, version):
+        if self.dateTexte is None:
+            raise Exception()
+
+        version.title = self.dom('.titreArt').text()
+        version.body = self.dom('.corpsArt').text()
+        version.histo = self.dom('.histoArt').text()
+
+
+    def get_article_version_list(self):
+        version_list = []
+        for link in self.dom('#left_menu .pGauche:eq(1) a'):
+            query = Q(link).attr('href').split('?')[1]
+            version_list.append(parse_qs(query)['dateTexte'][0].strip())
+        return version_list
