cb48b91d09d00c4dd79cd6f03fddeac81508fbec
Guillaume Gentile <ewandor@dorfsvald.net>
2014-06-25 13:05:29 +0200

    Separating FrenchLaw model and Legifrance client in different package

---
+++ FrenchLawModel/__init__.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+version_status = Enum('in_effect', 'modificated', 'abrogated')
+
+class Text(object)
+    def __init__(self):
+        self.articles = []
+
+    def append(self, article):
+        self.articles.append(article)
+
+    def get_laws(self):
+        laws = []
+        for article in self.articles:
+            laws.append(article.version.law)
+
+class Article(object):
+    def __init__(self):
+        self.versions = []
+
+    def add_version(self, version):
+        self.versions.append(version)
+
+    def get_status(self):
+        return self.versions[-1].status
+
+
+class Version(object):
+    pass
